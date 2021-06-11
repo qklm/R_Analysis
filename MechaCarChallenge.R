c6b8bd2271ef33a92118ee9c901126efcf5111af
David Brown <quinsoq@gmail.com>
2021-06-11 08:06:10 -0700

    deliverable 1 code updated

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
 library(dplyr)
-mechacar <- read.csv(MechaCar_mpg.csv)
-lm(qsec ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mechacar) #generate multiple linear regression model
-summary(lm(qsec ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mechacar) #generate multiple linear regression model)
+mechacar <- read.csv(file='MechaCar_mpg.csv')
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar))
