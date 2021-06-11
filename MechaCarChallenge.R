e1a75e77c0e0a04b4bb9e549f2b2b9e41ef35e73
David Brown <quinsoq@gmail.com>
2021-06-11 09:08:49 -0700

    summary data images, & summary data code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,11 @@
 library(dplyr)
 mechacar <- read.csv(file='MechaCar_mpg.csv')
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar) #generate multiple linear regression model
+# Linear model to test for nonrandom factors
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar))
+# Loading in suspension coil
+suscoil <- read.csv(file='Suspension_Coil.csv')
+# Summary of PSI in dataset
+total_summary <- suscoil  %>% summarise(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), StD = sqrt(var(PSI)))
+# Dividing summary data into lots
+lot_summary  <- suscoil  %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), StD = sqrt(var(PSI)))
